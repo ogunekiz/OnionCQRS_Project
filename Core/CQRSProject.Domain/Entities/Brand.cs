09fde8983b920c2caeff92450a5cf4b155474c81
Ogün EKİZ <ogunekiz@hotmail.com>
2024-09-29 11:46:38 +0300

    Entity'ler oluşturuldu.

---
+++ Core/CQRSProject.Domain/Entities/Brand.cs
@@ -0,0 +1,18 @@
+﻿using CQRSProject.Domain.Common;
+
+namespace CQRSProject.Domain.Entities
+{
+    public class Brand : EntityBase
+    {
+        public Brand()
+        {
+        }
+
+        public Brand(string name)
+        {
+            Name = name;
+        }
+
+        public required string Name { get; set; }
+    }
+}
---
+++ Core/CQRSProject.Domain/Entities/Detail.cs
@@ -0,0 +1,25 @@
+﻿using CQRSProject.Domain.Common;
+
+namespace CQRSProject.Domain.Entities
+{
+    public class Detail : EntityBase
+    {
+        public Detail()
+        {
+        }
+
+        public Detail(string title, string description, int categoryId, Category category)
+        {
+            Title = title;
+            Description = description;
+            CategoryId = categoryId;
+            Category = category;
+        }
+
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
